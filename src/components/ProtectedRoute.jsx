b509e49839917671f59c94b650939fdabe5db80c
edu1694 <eduardocardenas.16941694@gmail.com>
2024-09-28 05:02:03 -0300

    Fix error y Agrear paciente
    Se agrega la pagina para agregar a los paciente se  fixea un error en la pagina HomePage que al cerrar sesion no te mandaba a la HomePage

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 
 function ProtectedRoute({ children }) {
-  const isAuthenticated = localStorage.getItem('isAuthenticated');
+  const [isAuthenticated, setIsAuthenticated] = useState(null); // Estado inicial como null
+  const location = useLocation();
 
-  if (!isAuthenticated) {
-    return <Navigate to="/" />;
+  // Verifica la autenticación al montar el componente
+  useEffect(() => {
+    const auth = localStorage.getItem('isAuthenticated');
+    setIsAuthenticated(auth === 'true'); // Solo se ejecuta una vez
+  }, []); // El array vacío asegura que este efecto solo se ejecute una vez
+
+  // Muestra un indicador de carga mientras se verifica la autenticación
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
   }
 
+  // Redirigir si no está autenticado y no está en la página de inicio de sesión
+  if (!isAuthenticated && location.pathname !== '/') {
+    return <Navigate to="/" replace />;
+  }
+
+  // Renderiza los hijos si está autenticado o ya está en la página de inicio de sesión
   return children;
 }
 
